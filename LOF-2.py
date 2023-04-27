5bc3ca8a7d961953b551126d75c01f9b958fae25
Aahna <83895816+Aahna1909@users.noreply.github.com>
2023-04-27 22:43:32 +0530

    Create LOF-2.py

---
+++ LOF-2.py
@@ -0,0 +1,70 @@
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from urllib.request import urlretrieve
+import numpy as np
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import DBSCAN
+from sklearn.neighbors import NearestNeighbors
+import scipy.stats as st
+import warnings
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+warnings.filterwarnings('ignore')
+%matplotlib inline
+
+urlretrieve('https://raw.githubusercontent.com/Aahna1909/data-Analysis-AQI/main/Sample.csv%20-%20Sheet1.csv', '2018.csv')
+df = pd.read_csv('2018.csv')
+df.head()
+
+df.dtypes
+
+df.drop(['StationId', 'NO', 'NOx', 'NH3', 'Benzene', 'Xylene', 'Toluene','Date'], axis=1, inplace=True)
+df = df.dropna()
+aqi_map = {'Good': 0, 'Satisfactory': 1, 'Moderate': 2, 'Poor': 3, 'Very Poor': 4,'Severe': 5}
+df['AQI_Bucket'] = df['AQI_Bucket'].map(aqi_map)
+data = df.values
+
+# split into input and output elements
+X, y = data[:, :-1], data[:, -1]
+
+# split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+
+# identify outliers in the training dataset
+lof = LocalOutlierFactor()
+yhat = lof.fit_predict(X_train)
+
+# select all rows that are not outliers
+mask = yhat != -1
+X_train_inliers, y_train_inliers = X_train[mask, :], y_train[mask]
+
+# select all rows that are outliers
+X_train_outliers, y_train_outliers = X_train[yhat == -1, :], y_train[yhat == -1]
+
+# fit the model using inliers only
+model = LinearRegression()
+model.fit(X_train_inliers, y_train_inliers)
+
+# evaluate the model
+yhat = model.predict(X_test)
+
+# evaluate predictions
+mae = mean_absolute_error(y_test, yhat)
+print('MAE: %.3f' % mae)
+
+# plot inliers and outliers using a scatterplot
+plt.scatter(X_train_inliers[:, 0], X_train_inliers[:, 1], color="k", s=3.0, label="Inliers")
+plt.scatter(X_train_outliers[:, 0], X_train_outliers[:, 1], color="r", s=5.0, label="Outliers")
+plt.xlabel("PM2.5")
+plt.ylabel("AQI_Bucket")
+plt.title("Identifying Outliers using Local Outlier Factor")
+legend = plt.legend(loc="upper left")
+legend.legendHandles[0]._sizes = [10]
+legend.legendHandles[1]._sizes = [20]
+plt.show()
